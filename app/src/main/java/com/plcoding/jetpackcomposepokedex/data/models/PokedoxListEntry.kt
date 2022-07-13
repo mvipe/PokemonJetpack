555002bf56aae55dd5d9b0ef6cb9071371cb5e82
mvipe <mohnishvats@gmail.com>
2022-07-13 15:24:21 +0530

    made Pokemon List Screen ui and Pokemon Row ui

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/models/PokedoxListEntry.kt
@@ -0,0 +1,8 @@
+package com.plcoding.jetpackcomposepokedex.data.models
+
+data class PokedoxListEntry (
+    val pokemonName: String,
+    val imageUrl:String,
+    val number: Int
+
+    )
--- app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemonList/PokemonListScreen.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemonList/PokemonListScreen.kt
@@ -2,9 +2,12 @@ package com.plcoding.jetpackcomposepokedex.pokemonList
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,19 +16,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavController
+import coil.request.ImageRequest
+import com.google.accompanist.coil.CoilImage
 import com.plcoding.jetpackcomposepokedex.R
-
+import com.plcoding.jetpackcomposepokedex.data.models.PokedoxListEntry
+import com.plcoding.jetpackcomposepokedex.ui.theme.RobotoCondensed
 
 
 @Composable
@@ -48,7 +63,8 @@ fun PokemonListScreen(
             
             SearchBar(
                 hint = "Search ...",
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(16.dp)){
 
             }
@@ -97,5 +113,91 @@ fun SearchBar(
             modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
                 )
         }
+    }
+}
+
+@Composable
+fun PokedoxEntry(
+    entry: PokedoxListEntry,
+    navController: NavController,
+    modifier: Modifier=Modifier,
+    viewModel: PokemonListViewModel= hiltNavGraphViewModel()
+){
+    val defaultDominantColor=MaterialTheme.colors.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(contentAlignment = Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor,
+                        defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "pokemon_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+                )
+            }
+    ){
+        Column() {
+            CoilImage(request = ImageRequest.Builder(LocalContext.current)
+                .data(entry.imageUrl)
+                .target{
+                    viewModel.calculateDominantColor(it){
+                        dominantColor=it
+                    }
+                }.build(),
+                contentDescription = entry.pokemonName,
+                fadeIn = true,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            ) {
+                CircularProgressIndicator(
+                    color = MaterialTheme.colors.primary,
+                    modifier = Modifier.scale(0.5f)
+                )
+                
+                Text(text = entry.pokemonName,
+                     fontFamily = RobotoCondensed,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    modifier=Modifier.fillMaxWidth())
+            }
+        }
+    }
+}
+
+@Composable
+fun PokedoxRow(
+    rowIndex:Int,
+    entries:List<PokedoxListEntry>,
+    navController: NavController,
+    
+){
+    Column() {
+        Row() {
+            PokedoxEntry(entry = entries[rowIndex*2], navController = navController,
+            modifier = Modifier.weight(1f))
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            if(entries.size>=rowIndex*2+2){
+                PokedoxEntry(entry = entries[rowIndex*2+1], navController = navController,
+                    modifier = Modifier.weight(1f))
+            }else{
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
